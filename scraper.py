96d41fd2e5331eefe0ae84890a47126add3e5ff1
Masrt200 <immasrt@gmail.com>
2020-07-29 16:27:25 +0530

    ctfd done

--- scraper.py
+++ scraper.py
@@ -10,8 +10,11 @@ from progress.bar import ShadyBar
 
 import argparse 
 
+
 def connect(url):
+	#using stored session cache for connection
 	if os.path.exists(".cache") and not args.dynamic_login:
+		download()
 		cj=cookies.RequestsCookieJar()
 		with open(".cache","rb") as f:
 			cook=pickle.load(f)
@@ -22,6 +25,7 @@ def connect(url):
 	elif not os.path.exists(".cache") and not args.dynamic_login:
 		print(lc+".cache missing! create a new session!!"+rt)
 		sys.exit()
+	#new connection
 	else:
 		with Session() as s:
 			print(lc+bd+"Connecting...\r"+rt,end='')
@@ -48,6 +52,7 @@ def connect(url):
 
 			return s
 
+#scraping the files
 def scrape_challs(s,limit,no):
 	for i in range(limit*no,limit*(no+1)):
 		try:
@@ -65,27 +70,27 @@ def scrape_challs(s,limit,no):
 			pass
 		bar.next()
 
+#making directories and downloading the files
 def local(exd):
 	if exd['hints']==[]: exd['hints']='None'
 	master_path=open(".path").read()
 
+	if not os.path.exists(master_path):
+		pathe(master_path)
+
 	if not os.path.exists(master_path+"./{}".format(exd['category'])):
 		pathe(master_path+"./{}".format(exd['category']))
-		#process=subprocess.Popen(["mkdir","./{}".format(exd['category'])], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		#process.communicate()
 
-	template="# {}\n\n### Points: {}\n\n### Desciption:\n{}\n\n>Hints: {}\n\n#### tags: {}".format(exd['name'],exd['value'],exd['description'],exd['hints'],exd['tags'])
+	template="# {}\n\n### Points: {}\n\n### Desciption:\n{}\n\n>Hints: {}\n\n#### tags: {}".format(exd['name'],exd['value'],exd['description'],exd['hints'],' '.join(exd['tags']))
 	path=master_path+"./{}/{}/".format(exd['category'],exd['name'].replace(' ','_'))
 	
 	pathe(path)
-	#process=subprocess.Popen(["mkdir",path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	#process.communicate()
 
 	with open(path+"description.md",'w') as f:
 		f.write(template)
 	f.close()
 
-	if exd['files'] !=[] and exd['category']=='Forensics':
+	if exd['files'] !=[] and (exd['category'] in downloads or 'A' in downloads):
 		for i in exd['files']:
 			file=url+i
 			filename=re.findall(r'/(.*)\?token',file)[0].split('/')[-1]
@@ -96,26 +101,48 @@ def pathe(path):
 	process=subprocess.Popen(["mkdir",path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	process.communicate()
 
+def download():
+	global downloads
+	downloads=input(lg+"@download files? (input seperated by spaces, exact)\n(All: A,None: N,specific: Crypto Forensics etc): "+rt)
+	downloads=downloads.split(' ')
+	if 'N' in downloads: downloads=[]
 
 class SolverThread(threading.Thread):
 	def run(self):
 		thread_number = int(threading.currentThread().getName())
-		scrape_challs(s,100//threads,thread_number)
-
-
+		scrape_challs(s,round(100/threads+0.5),thread_number)
 
 def initialize_parser():
 	
 	parser=argparse.ArgumentParser(description="Capturing the Scrapes")
-	#parser.add_argument('--creds','-c',help="use default creds",action="store_true")
-	parser.add_argument('--update','-p',help="if the website updates, use this",action="store_true")
-	#parser.add_argument('--cache','-c',help="use stored cache to extract data",action="store_true")
 	parser.add_argument('--dynamic_login','-d',help="create a new session for a ctf",action="store_true")
 	parser.add_argument('--threads','-t',help="number of threads to use (default -> 8)",type=str)
-	#parser.add_argument('--CTFS','--ctfs','-C',help="displays the upcoming CTFS for you to choose from",action="store_true")
+	
 	return parser
 
 def main():
+	global username,password,url,threads,s,master_path,bar,args
+
+	parser=initialize_parser()
+	args=parser.parse_args()
+
+	if args.dynamic_login:
+		username=input(lg+"@username: "+rt)
+		password=input(lg+"@password: "+rt)
+		url=input(lg+"@url: "+rt).rstrip('/')
+		master_path=input(lg+"@scrape to? (relative path): "+rt)+"/"
+		download()
+
+		f=open(".path","w").write(master_path)
+		if not os.path.exists("master_path"):
+			pathe(master_path)
+
+	if args.threads !=None:
+		threads=int(args.threads)
+
+	s=connect(url)
+	bar = ShadyBar(pk+bd+'Scraping Data'+rt,fill=lg+">"+rt,suffix=lc+'%(percent)d%% - %(elapsed)ds'+rt,max=round(100/threads+0.5)*threads)
+
 	thread=[]
 	for i in range(threads):
 		string=SolverThread(name = "{}".format(i))
@@ -124,13 +151,20 @@ def main():
 	for i in thread:
 		i.join()
 
-#defaults
+	print(lr+"\nFinished!"+rt)
+
+#defaults (change as you wish and the comment out the required input fields in args.dynamic_login)
 username="Masrt"
 password="12345678"
 url="https://ctf.csivit.com"
-
+downloads=["A"] #must be a string array
+s=None
+bar=None
+args=None
+master_path="../csictf"
 threads=8
-#url=''
+
+#colours
 lg='\033[92m'
 rt='\033[0m'
 lr='\033[91m'
@@ -139,29 +173,5 @@ pk='\033[95m'
 bd='\033[01m'
 og='\033[33m'
 
-parser=initialize_parser()
-args=parser.parse_args()
-
-if args.dynamic_login:
-	username=input(lg+"@username: "+rt)
-	password=input(lg+"@password: "+rt)
-	url=input(lg+"@url: "+rt).rstrip('/')
-	master_path=input(lg+"@scrape to? (relative path): "+rt)+"/"
-	f=open(".path","w").write(master_path)
-	if not os.path.exists("master_path"):
-		pathe(master_path)
-
-
-if args.threads !=None:
-	threads=int(args.threads)
-#elif args.cache:
-
-s=connect(url)
-bar = ShadyBar(pk+bd+'Scraping Data'+rt,fill=lg+">"+rt,suffix=lc+'%(percent)d%% - %(elapsed)ds'+rt,max=100//threads*threads)
-
-main()
-print(lr+"\nFinished!"+rt)
-
-
-#choice
-#where to save
+if __name__ == '__main__':
+	main()
